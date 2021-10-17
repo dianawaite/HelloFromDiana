b11b34674b9618ccc97ca961ab444f5f6bf81e62
Kara Stevens <hog16002@byui.edu>
2021-10-16 22:14:35 -0600

    Adding my greeting!

--- src/hello.java
+++ src/hello.java
@@ -3,6 +3,8 @@ class hello {
 
     public static void main(String[] args) {
         String firstName = "Diana";
+        String firstName2 = "Kara";
     System.out.printf("my name is %s", firstName);
+    System.out.printf("Hi Diana, my name is %s", firstName2);
     }
 }
